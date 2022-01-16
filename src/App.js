d7dd64f9eed2cc2ee88fbfb634626a0da699ee7e
Vitaly Semenov <mtgmind@gmail.com>
2022-01-16 20:34:36 +0100

    Added cart context and provider for context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import {useState} from "react";
+import CartProvider from "./store/CartProvider";
 
 function App() {
     const [showCart, setShowCart] = useState(false);
@@ -15,13 +16,13 @@ function App() {
     };
 
     return (
-    <>
+    <CartProvider>
         {showCart && <Cart onHideCart={hideCartHandler} />}
         <Header onShowCart={showCartHandler}/>
         <main>
             <Meals />
         </main>
-    </>
+    </CartProvider>
   );
 }
 
