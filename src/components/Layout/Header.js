f1300ac499cb70e8df1530ecbc5e3708f96777f1
Vitaly Semenov <mtgmind@gmail.com>
2022-01-16 13:40:06 +0100

    Added header button and som bug fixes in css

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import classes from './Header.module.css';
 import headerImage from '../../assets/meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
     return (
         <>
             <header className={classes.header}>
                 <h1>Meals App</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src={headerImage} alt="A table full of food"/>
