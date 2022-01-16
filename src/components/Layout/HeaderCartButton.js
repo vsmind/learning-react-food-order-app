dcce74b3191c72091e64b2a4606eaf16077dd9ff
Vitaly Semenov <mtgmind@gmail.com>
2022-01-16 20:41:13 +0100

    Using context to display number of items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,19 @@
 import classes from './HeaderCartButton.module.css'
 import CartIcon from "../Cart/CartIcon";
+import {useContext} from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
 
     const showCartHandler = () => {
         props.onShowCart(true);
     };
+
+    const cartContext = useContext(CartContext);
+
+    const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
 
     return (
         <button className={classes.button} onClick={showCartHandler}>
@@ -16,7 +24,7 @@ const HeaderCartButton = (props) => {
                 Your Cart
             </span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
