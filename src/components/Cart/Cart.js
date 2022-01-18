925a7eb90a301ef8ed9193696c77aaf833a52f9d
Vitaly Semenov <mtgmind@gmail.com>
2022-01-18 23:12:31 +0100

    Making items removable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,11 +8,11 @@ const Cart = (props) => {
     const cartContext = useContext(CartContext);
 
     const cartItemRemoveHandler = (id) => {
-
+        cartContext.removeItem(id)
     }
 
     const cartItemAddHandler = (item) => {
-
+        cartContext.addItem({...item, amount: 1});
     }
 
     const cartItems = <ul className={classes['cart-items']}>
