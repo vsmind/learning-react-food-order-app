d139aaaabadcd0bbf034c074bb030cafa8794464
Vitaly Semenov <mtgmind@gmail.com>
2022-01-18 23:24:05 +0100

    Use side effect for button animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,24 +1,41 @@
 import classes from './HeaderCartButton.module.css'
 import CartIcon from "../Cart/CartIcon";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-
+    const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
     const showCartHandler = () => {
         props.onShowCart(true);
     };
 
     const cartContext = useContext(CartContext);
+    const {items} = cartContext;
 
-    const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
+    const buttonClass = `${classes.button} ${buttonIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setButtonIsHighlighted(true);
+        const timer = setTimeout(() => {
+            setButtonIsHighlighted(false)
+        }, 300);
+        // Clean up function for useEffect
+        return () => {
+            clearTimeout(timer)
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={showCartHandler}>
+        <button className={buttonClass} onClick={showCartHandler}>
             <span className={classes.icon}>
-                <CartIcon />
+                <CartIcon/>
             </span>
             <span>
                 Your Cart
