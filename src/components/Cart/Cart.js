d3d8971aec209777622338ef39d21bad2174904c
Vitaly Semenov <mtgmind@gmail.com>
2022-01-18 22:47:51 +0100

    Fixed output to cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,37 @@
 import classes from './Cart.module.css';
 import Modal from "../UI/Modal";
+import {useContext} from "react";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem/CartItem";
 
 const Cart = (props) => {
-    const cartItems = <ul className={classes['cart-items']}>{[{
-        id: 'c1',
-        name: 'Sushi',
-        amount: '2',
-        price: '9.99'
-    }].map(item => <li key="c1">{item.name}</li>)}</ul>;
+    const cartContext = useContext(CartContext);
+
+    const cartItemRemoveHandler = (id) => {
+
+    }
+
+    const cartItemAddHandler = (item) => {
+
+    }
+
+    const cartItems = <ul className={classes['cart-items']}>
+        {cartContext.items.map(item =>
+            <CartItem key={item.id}
+                      name={item.name}
+                      price={item.price}
+                      amount={item.amount}
+                      onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                      onAdd={cartItemAddHandler.bind(null, item)}
+            />
+        )}
+    </ul>;
+
+    const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+    const hasItems = cartContext.items.length > 0
 
     const sendOrderHandler = () => {
-      console.log(cartItems);
+        console.log(cartItems);
     };
 
     return (
@@ -18,11 +39,11 @@ const Cart = (props) => {
             {cartItems}
             <div className={classes.total}>
                 <span>Total Ammout</span>
-                <span>20</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-                <button className={classes.button} onClick={sendOrderHandler}>Order</button>
+                {hasItems && <button className={classes.button} onClick={sendOrderHandler}>Order</button>}
             </div>
         </Modal>
     );
